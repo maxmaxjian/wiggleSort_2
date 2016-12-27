517e2578ab64289ef16783a863a982491f0b84e6
Jian Wei <jianwei@jianwei-deb8-64>
2016-12-27 15:26:14 -0500

    second version

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@
 
 class solution {
   public:
-    // // O(n^2)
+    // // O(n^2) solution
     // void wiggleSort(std::vector<int> & nums) {
     //     size_t curr = 0;
     //     while (curr < nums.size()) {
@@ -21,39 +21,58 @@ class solution {
     //     }
     // }
 
-    void wiggleSort(std::vector<int> & nums) {
-        size_t mid = nums.size()/2;
+    // O(n) solution
+    void wiggleSort(std::vector<int> & nums) {        
+    //     size_t mid = nums.size()/2;
+    //     std::nth_element(nums.begin(), nums.begin()+mid, nums.end());
+    //     std::cout << std::endl;
+    //     std::copy(nums.begin(), nums.end(), std::ostream_iterator<int>(std::cout," "));
+    //     std::cout << std::endl;
+
+    //     size_t first, second;
+    //     if (nums.size()%2 == 0) {
+    //         first = 1, second = mid;
+    //         while (second < nums.size()) {
+    //             std::swap(nums[first], nums[second]);
+    //             std::copy(nums.begin(), nums.end(), std::ostream_iterator<int>(std::cout," "));
+    //             std::cout << std::endl;
+    //             first += 2;
+    //             second++;
+    //         }
+    //     }
+    //     else {
+    //         first = 1, second = mid+1;
+    //         while (second < nums.size()) {
+    //             std::swap(nums[first], nums[second]);
+    //             std::copy(nums.begin(), nums.end(), std::ostream_iterator<int>(std::cout," "));
+    //             std::cout << std::endl;
+    //             first += 2;
+    //             second++;
+    //         }
+    //     }
+
+        size_t mid = nums.size()%2==0 ? nums.size()/2 : nums.size()/2+1;
         std::nth_element(nums.begin(), nums.begin()+mid, nums.end());
         std::cout << std::endl;
         std::copy(nums.begin(), nums.end(), std::ostream_iterator<int>(std::cout," "));
         std::cout << std::endl;
 
-        size_t first, second;
-        if (nums.size()%2 == 0) {
-            first = 1, second = mid;
-            while (second < nums.size()) {
-                std::swap(nums[first], nums[second]);
-                std::copy(nums.begin(), nums.end(), std::ostream_iterator<int>(std::cout," "));
-                std::cout << std::endl;
-                first += 2;
-                second++;
-            }
-        }
-        else {
-            first = 1, second = mid+1;
-            while (second < nums.size()) {
-                std::swap(nums[first], nums[second]);
-                std::copy(nums.begin(), nums.end(), std::ostream_iterator<int>(std::cout," "));
-                std::cout << std::endl;
-                first += 2;
-                second++;
-            }
+        size_t first = 1, second = mid;
+        while (second < nums.size()) {
+            size_t i = second+1;
+            while (i < nums.size() && nums[second] == nums[first-1])
+                std::swap(nums[second], nums[i++]);
+            std::swap(nums[first], nums[second]);
+            std::copy(nums.begin(), nums.end(), std::ostream_iterator<int>(std::cout," "));
+            std::cout << std::endl;
+            first += 2;
+            second++;
         }
     }
 };
 
 int main() {
-    // std::vector<int> nums{1,5,1,1,6,4};
+    // std::vector<int> nums{1,5,1,1,6,2};
     std::vector<int> nums{1,3,2,2,3,1};
     
     std::cout << "Before wiggle sorting:\n";
